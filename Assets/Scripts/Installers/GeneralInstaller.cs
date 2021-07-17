8e43b23ea1e55cf6204fe2c557dcda39496c5430
gribdmitriy <dmitrii.9rib@gmail.com>
2021-07-18 01:58:38 +0300

    FPS counter moved to dont destroy on load

---
+++ Assets/Scripts/Installers/GeneralInstaller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Installers
+{
+    public class GeneralInstaller : MonoInstaller
+    {
+        [SerializeField] private GameObject fpsCounter;
+
+        public override void InstallBindings()
+        {
+            var debug = CreateDebugParent();
+
+            InitializeFpsCounter(debug);
+        }
+
+        private static GameObject CreateDebugParent()
+        {
+            var debug = new GameObject {name = "Debug"};
+            DontDestroyOnLoad(debug);
+            return debug;
+        }
+
+        private void InitializeFpsCounter(GameObject debug)
+        {
+            Container.InstantiatePrefab(fpsCounter)
+                .transform.SetParent(debug.transform);
+        }
+    }
+}
