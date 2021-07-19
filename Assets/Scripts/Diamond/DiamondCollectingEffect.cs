886e2784c37d5e75798ed3c4078b65bfe85da405
gribdmitriy <dmitrii.9rib@gmail.com>
2021-07-19 19:07:58 +0300

    pfff....

---
+++ Assets/Scripts/Diamond/DiamondCollectingEffect.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class DiamondCollectingEffect : MonoBehaviour
+{
+    [SerializeField] private GameObject effect;
+
+    private AudioSource audioSource;
+
+    public void BindAudioSource(AudioSource audioSource)
+    {
+        this.audioSource = audioSource;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.CompareTag("Untagged"))
+        {
+     
+            GetComponent<BoxCollider>().enabled = false;
+            Instantiate(effect).transform.position = transform.position;
+            Destroy(gameObject);
+        }
+    }
+}
