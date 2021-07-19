a0ae8086862861f152399693482d8ae4eec5e93e
gribdmitriy <dmitrii.9rib@gmail.com>
2021-07-19 19:18:02 +0300

    Add sounds for diamonds and collecting effect

---
+++ Assets/Scripts/Diamond/DiamondsController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Diamond
+{
+    public class DiamondsController : MonoBehaviour
+    {
+        private void Start()
+        {
+            var effects = transform.GetComponentsInChildren<DiamondCollectingEffect>();
+            foreach (var effect in effects)
+            {
+                effect.BindAudioSource(GetComponent<AudioSource>());
+            }
+        }
+    }
+}
