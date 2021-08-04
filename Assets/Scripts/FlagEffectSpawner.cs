c773163b55bfd2b9928ef2dbc183a611e4c891e5
gribdmitriy <dmitrii.9rib@gmail.com>
2021-08-04 21:31:31 +0300

    Flag implemented

--- Assets/Scripts/FlagEffectSpawner.cs
+++ Assets/Scripts/FlagEffectSpawner.cs
@@ -6,11 +6,10 @@ public class FlagEffectSpawner : MonoBehaviour
 {
     [SerializeField] private Transform point;
     [SerializeField] private GameObject effect;
-    [SerializeField] private AudioSource audioSource;
-    
+
     public void CreateEffect()
     {
         Instantiate(effect, point.position, Quaternion.identity, point);
-        audioSource.Play();
+        
     }
 }
--- Assets/Scripts/FlagRoad.cs
+++ Assets/Scripts/FlagRoad.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class FlagRoad : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource audioSource;
     
     private void OnCollisionEnter(Collision other)
     {
         if (other.collider.CompareTag("DiamondCollector") || other.collider.CompareTag("Untagged"))
         {
             animator.Play("FlagAnim");
+            audioSource.Play();
         }
     }
 }
