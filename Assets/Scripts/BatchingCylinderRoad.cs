6bb66e71ee7f64b335915cab4914c447904d1bec
gribdmitriy <dmitrii.9rib@gmail.com>
2021-10-24 22:07:45 +0300

    Environment materials

---
+++ Assets/Scripts/BatchingCylinderRoad.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatchingCylinderRoad : MonoBehaviour
+{
+    [SerializeField] private bool batchingEnabled;
+    void Awake()
+    {
+        if (batchingEnabled) StaticBatchingUtility.Combine(gameObject);
+    }
+}
