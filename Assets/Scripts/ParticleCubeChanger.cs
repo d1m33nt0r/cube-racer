facf62520c58d6cc5ceef2f9e529500814eb5b46
gribdmitriy <dmitrii.9rib@gmail.com>
2021-12-08 01:10:01 +0200

    no message

---
+++ Assets/Scripts/ParticleCubeChanger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Zenject;
+
+namespace DefaultNamespace
+{
+    public class ParticleCubeChanger : MonoBehaviour
+    {
+        [SerializeField] private ParticleSystem m_particleSystem;
+
+        [Inject]
+        private void Construct(ThemeManager.ThemeManager _themeManager)
+        {
+            m_particleSystem.GetComponent<Renderer>().material = _themeManager.GetCurrentBoxTheme();
+        }
+    }
+}
