b7de504fa87cf553f94b835abba85358ed1ed000
gribdmitriy <dmitrii.9rib@gmail.com>
2021-12-14 17:35:23 +0200

    polishing magnit effect

--- Assets/Scripts/Player/MagnitPlayer.cs
+++ Assets/Scripts/Player/MagnitPlayer.cs
@@ -7,7 +7,15 @@ public class MagnitPlayer : MonoBehaviour
     [SerializeField] private BoxCollider magnitCollider;
     [SerializeField] private ParticleSystem effect;
     [SerializeField] private GameObject magnit;
-    
+
+    private Transform startMarker;
+    public Transform endMarker;
+
+ 
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+
     public void EnableMagnitPlayerAndDestroyMagnitOnMap(GameObject gameObject)
     {
         Destroy(gameObject);
@@ -36,7 +44,24 @@ public class MagnitPlayer : MonoBehaviour
 
     private void MoveBoxIntoPlayer(Transform box)
     {
-        var movePosition = transform.parent.parent.GetComponentInChildren<BoxController>().GetBoxPositionXYZ();
-        box.DOMove(new Vector3(movePosition.x, box.position.y, movePosition.z), 0);
+        startMarker = box;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+        StartCoroutine(Move());
+        //var movePosition = transform.parent.parent.GetComponentInChildren<BoxController>().GetBoxPositionXYZ();
+        //box.DOMove(new Vector3(movePosition.x, box.position.y, movePosition.z), 0);
+    }
+
+    private IEnumerator Move()
+    {
+        while (startMarker.position != endMarker.position)
+        {
+            if (startMarker.childCount == 0) break;
+            float distCovered = (Time.time - startTime) * speed;
+            float fractionOfJourney = distCovered / journeyLength;
+            startMarker.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
+            Debug.Log("hop hei lalaley!");
+            yield return null;
+        }
     }
 }
