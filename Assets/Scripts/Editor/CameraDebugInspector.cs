b5549af860aba160c6c8284d2c662eff57323261
gribdmitriy <dmitrii.9rib@gmail.com>
2021-12-18 13:02:58 +0200

    no message

---
+++ Assets/Scripts/Editor/CameraDebugInspector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(CameraDebug))]
+public class CameraDebugInspector : Editor
+{
+    private CameraDebug cameraDebug;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        var cameraDebug = (CameraDebug) target;
+
+        if (GUILayout.Button("Scale field view"))
+        {
+            cameraDebug.ScaleFieldView();
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/CustomSnappingTool/CustomSnappingTool.cs
@@ -0,0 +1,90 @@
+using System;
+using UnityEditor;
+using UnityEditor.EditorTools;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+
+[EditorTool("Custom Snap Move", typeof(CustomSnap))]
+public class CustomSnappingTool : EditorTool
+{
+    public Texture2D ToolIcon;
+
+    private Transform oldTarget;
+    private CustomSnapConnector[] allPoints;
+    private CustomSnapConnector[] targetPoints;
+
+    public override GUIContent toolbarIcon
+    {
+        get
+        {
+            return new GUIContent
+            {
+                image = ToolIcon,
+                text = "Custom Snap Move Tool",
+                tooltip = "Custom Snap Move Tool"
+            };
+        }
+    }
+
+    public override void OnToolGUI(EditorWindow window)
+    {
+        var targetTransform = ((CustomSnap) target).transform;
+
+        if (targetTransform != oldTarget)
+        {
+            var prefabStage = PrefabStageUtility.GetPrefabStage(targetTransform.gameObject);
+
+            if (prefabStage != null)
+                allPoints = prefabStage.prefabContentsRoot.GetComponentsInChildren<CustomSnapConnector>();
+            else
+                allPoints = FindObjectsOfType<CustomSnapConnector>();
+            
+            targetPoints = targetTransform.GetComponentsInChildren<CustomSnapConnector>();
+
+            oldTarget = targetTransform;
+        }
+        
+        EditorGUI.BeginChangeCheck();
+
+        var newPosition = Handles.PositionHandle(targetTransform.position, Quaternion.identity);
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(targetTransform, "Move with custom snap");
+            MoveWithSnapping(targetTransform, newPosition);
+        }
+    }
+
+    private void MoveWithSnapping(Transform targetTransform, Vector3 newPosition)
+    {
+        var bestPosition = newPosition;
+
+        var closesDistance = float.PositiveInfinity;
+
+        foreach (var point in allPoints)
+        {
+            if (point.transform.parent == targetTransform) continue;
+
+            foreach (var ownPoint in targetPoints)
+            {
+                var targetPos = point.transform.position - (ownPoint.transform.position - targetTransform.position);
+                var distance = Vector3.Distance(targetPos, newPosition);
+
+                if (distance < closesDistance)
+                {
+                    closesDistance = distance;
+                    bestPosition = targetPos;
+                }
+            }
+        }
+
+        if (closesDistance < 0.5f)
+        {
+            targetTransform.position = bestPosition;
+        }
+        else
+        {
+            targetTransform.position = newPosition;
+        }
+    }
+}
